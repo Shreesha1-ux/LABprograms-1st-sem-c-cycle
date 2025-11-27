909b736660da73d30b5dc177c3fe8a16a506ed84
Shreeshaa_F01 <shreeshapoojary100@gmail.com>
2025-11-27 19:39:06 +0530

    Implement character case conversion in LabP1.c
    Add character case conversion functionality

--- LabP1.c
+++ LabP1.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    char str;
+    printf("enter your any charecter to convert : \n");
+    scanf("%c",&str);
+        if(str>=65 && str<=90)
+            printf("%c in lower case %c ",str,str+32);
+        if(str>=97 && str<=122)
+            printf("%c in uppercase case %c ",str,str-32);
+
+}
 
